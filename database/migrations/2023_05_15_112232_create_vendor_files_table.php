c46c1608eafc6b2b36668a8858d3b69a23539e1c
andiyudi <49513145+andiyudi@users.noreply.github.com>
2023-06-14 15:13:19 +0700

    add user seeder and update migration for file type

--- database/migrations/2023_05_15_112232_create_vendor_files_table.php
+++ database/migrations/2023_05_15_112232_create_vendor_files_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('vendor_id');
             $table->string('file_name');
             $table->string('file_path');
-            $table->tinyInteger('file_type')->comment('0: Compro, 1: Legalitas, 2: Hasil Survey');
+            $table->tinyInteger('file_type')->comment('0: Compro, 1: Legalitas, 2: Hasil Survey, 3: Blacklist');
             $table->timestamps();
 
             $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
--- database/migrations/2023_05_29_101247_create_procurement_files_table.php
+++ database/migrations/2023_05_29_101247_create_procurement_files_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('procurement_id');
             $table->string('file_name');
             $table->string('file_path');
-            $table->tinyInteger('file_type')->comment('0: File Selected Vendor, 1: File Cancelled Procurement');
+            $table->tinyInteger('file_type')->comment('0: File Selected Vendor, 1: File Cancelled Procurement, 2: File Repeat Procurement, 3: File Evaluation Company, 4: File Evaluation Vendor');
             $table->timestamps();
 
             $table->foreign('procurement_id')->references('id')->on('procurements')->onDelete('cascade');
